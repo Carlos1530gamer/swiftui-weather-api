ae1c785a980c961ab1693d46e75674eff47e635e
Carlos Daniel Hernandez Chauteco <gamercarlos1530@gmail.com>
2023-07-05 15:32:00 -0600

    Initial Commit

---
+++ Weather Demo/Weather_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_DemoApp.swift
+//  Weather Demo
+//
+//  Created by Carlos Daniel Hernandez Chauteco on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
